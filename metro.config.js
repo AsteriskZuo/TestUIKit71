9e53d884d9747cb030275e5b12dfeb0a73debdc8
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-05-11 15:23:27 +0800

    chore: add local npm package dev

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = {
+  watchFolders: [
+    // Add your project directories here
+    '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/packages/react-native-chat-uikit',
+  ],
+  resolver: {
+    extraNodeModules: {
+      // Add your project directories here
+      'react-native-chat-uikit':
+        '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/packages/react-native-chat-uikit',
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -15,3 +26,5 @@ module.exports = {
     }),
   },
 };
+
+console.log('test:zuoyu:config:', module.exports);
