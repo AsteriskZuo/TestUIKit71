58bafdaf5574879b2d073a39dcc35189feb3136a
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-05-12 17:30:02 +0800

    updagte

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 const name = 'react-native-chat-uikit';
 const source =
   '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/packages/react-native-chat-uikit/src/index';
-const source2 =
-  '/Users/asterisk/Downloads/2024-05-11/react-native-chat-uikit-2.1.0/src/index';
+// const source2 =
+//   '/Users/asterisk/Downloads/2024-05-11/react-native-chat-uikit-2.1.0/src/index';
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [name]: source2,
-        },
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     'module-resolver',
+  //     {
+  //       extensions: ['.tsx', '.ts', '.js', '.json'],
+  //       alias: {
+  //         [name]: source,
+  //       },
+  //     },
+  //   ],
+  // ],
 };
--- metro.config.js
+++ metro.config.js
@@ -7,19 +7,24 @@
 
 const source =
   '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/packages/react-native-chat-uikit';
-const source2 =
-  '/Users/asterisk/Downloads/2024-05-11/react-native-chat-uikit-2.1.0';
+// const source2 =
+//   '/Users/asterisk/Downloads/2024-05-11/react-native-chat-uikit-2.1.0';
 
 module.exports = {
   watchFolders: [
     // Add your project directories here
-    source2,
+    source,
   ],
   resolver: {
     extraNodeModules: {
       // Add your project directories here
-      'react-native-chat-uikit': source2,
+      // 'react-native-chat-uikit': source,
     },
+    nodeModulesPaths: [
+      // '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/packages/react-native-chat-uikit/node_modules',
+      // '/Users/asterisk/Codes/rn/react-native-chat-library-2.0/node_modules',
+      '/Users/asterisk/tmp/2024.05.11/TestUIKit71/node_modules',
+    ],
   },
   transformer: {
     getTransformOptions: async () => ({
